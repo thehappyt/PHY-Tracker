9e2f81dc07efe5039aba646f53b4c74e8bd585f6
Adam Thompson <thompson.adams+git@gmail.com>
2017-04-11 23:49:45 -0700

    Removed unneeded properties of returned array.

--- parseTrackerFile.js
+++ parseTrackerFile.js
@@ -1,5 +1,5 @@
 function parseTrackerFile(f) {
-    var i, j, l, m, c=[], txt;
+    var i, j, l, m, c=[], txt, cjmin, cjmax;
     txt = (f.text).split(/[\r\n]+/);
     for (i=1, l=txt.length; i<l; i++) txt[i]=txt[i].split(",");
     txt.splice(txt.length-1,1);
@@ -7,8 +7,8 @@ function parseTrackerFile(f) {
     for (j=0, m=txt[1].length; j<m; j++) { c.push([]); c[j].name=txt[1][j]; }
     for (i=2, l=txt.length, m=c.length; i<l; i++) for (j=0; j<m; j++) c[j].push(1*txt[i][j]);
     for (j=0, m=c.length; j<m; j++) {
-        c[j].min=min.apply(null, c[j]); c[j].max=max.apply(null, c[j]);
-        c[j].mid=(c[j].min+c[j].max)/2; c[j].dif=(c[j].max-c[j].min)/2;
+        cjmin=min.apply(null, c[j]); cjmax=max.apply(null, c[j]);
+        c[j].mid=(cjmin+cjmax)/2; c[j].dif=(cjmax-cjmin)/2;
     }
     return c;
 }
